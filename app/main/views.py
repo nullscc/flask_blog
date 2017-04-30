5f1332243c316d1f4cc4ca2add16ef6c48d64cdd
null <jarves@foxmail.com>
2017-04-30 12:26:57 +0800

    首页与文章页评论的链接

--- app/main/views.py
+++ app/main/views.py
@@ -43,7 +43,7 @@ def post(permalink):
         page, per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'],
         error_out=False)
     comments = pagination.items
-    return render_template('post.html', posts=[post], form=form,
+    return render_template('post.html', post=post, form=form,
                            comments=comments, pagination=pagination)
 
 @main.route('/new-post', methods=['GET', 'POST'])
